a8371d00c7013097e8ad46f4f7aa59e898a6754e
BrunoH <desabruno2004@gmail.com>
2024-06-04 23:18:00 -0300

    adicionando informacoes

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,6 +1,8 @@
 import lixos from '../assets/lixo-mar.jpg'
+import tartaruga from '../assets/foto-tartaruga.jpg'
 import Header from "../components/Header/Header";
 import Banner from "../components/Banner/Banner";
+import Conscientizar from '../components/Conscientizar/Conscientizar';
 
 
 export default function Home() {
@@ -12,6 +14,11 @@ export default function Home() {
     e inspiração. No entanto, estão enfrentando uma crise urgente: a poluição por plásticos.
     Milhões de toneladas de plástico são despejadas nos oceanos a cada ano, causando danos
     irreparáveis aos ecossistemas marinhos e à vida selvagem que depende deles.</Banner>
+
+    <Conscientizar titulo="Protegendo Nosso Oceano" img={tartaruga}>O oceano é um tesouro vasto e vital para 
+    a saúde do nosso planeta. Mas, infelizmente, tem sido tratado como um depósito de lixo, especialmente 
+    quando se trata de plásticos. Porém, a questão vai muito além de apenas poluir a paisagem marinha. 
+    Aqui estão algumas razões convincentes para não jogar plásticos no oceano:</Conscientizar>
     </>
   );
 }
