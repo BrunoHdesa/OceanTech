2fade1ae571bb52226f4beb3ffcb5aa5dfd52139
BrunoH <desabruno2004@gmail.com>
2024-06-05 01:35:45 -0300

    adicionando footer

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import style from '../Footer/Footer.module.css'
+
+const Footer = () => {
+    return(
+        <>
+            <footer className={style.footerStyle}>
+                <div>
+                    <p> Nome completo, RM, Turma</p>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
