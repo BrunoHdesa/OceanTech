7db70dfc027afc4bb8fca46486a64f80df55f97a
BrunoH <desabruno2004@gmail.com>
2024-06-06 22:54:05 -0300

    arrumando bug vercel

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <footer className={style.footerStyle}>
                 <div>
                     <p>Bruno Hermano de Sá - RM553799 - 1TDSPR</p>
-                    <p>Leonardo D' Ingianni Macedo - RM552753 - 1TDSPR</p>
+                    <p>Leonardo D&apos; Ingianni Macedo - RM552753 - 1TDSPR</p>
                     <p>Chrisman Campos - RM554026 - 1TDSPR</p>
                 </div>
             </footer>
